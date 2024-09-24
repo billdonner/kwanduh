fbe7dfc84e12aa0c23cdf6138c79fd44c9cd8b0b
bill donner <billdonner@gmail.com>
2024-09-24 10:56:52 -0500

    Initial Commit

---
+++ kwanduh/kwanduhApp.swift
@@ -0,0 +1,17 @@
+//
+//  kwanduhApp.swift
+//  kwanduh
+//
+//  Created by bill donner on 9/24/24.
+//
+
+import SwiftUI
+
+@main
+struct kwanduhApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
